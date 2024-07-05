0e6e17b753389a29546ba13407874de99b08c85a
Sergio Miranda <152328184+TitanDevelopMan@users.noreply.github.com>
2024-07-04 22:58:56 -0400

    ahora si cloudinary

--- medusa-config.js
+++ medusa-config.js
@@ -39,6 +39,8 @@ const CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
 const cloudinaryConfigured =
   CLOUDINARY_CLOUD_NAME && CLOUDINARY_API_KEY && CLOUDINARY_API_SECRET;
 
+// console.log(CLOUDINARY_CLOUD_NAME,CLOUDINARY_API_KEY,CLOUDINARY_API_SECRET);
+
 const fileServicePlugin = cloudinaryConfigured
   ? {
       resolve: `medusa-file-cloudinary`,
